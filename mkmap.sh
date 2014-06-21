4b02c332ac66428270da64a6b58511cb86e282ea
Steffen Möller <steffen_moeller@gmx.de>
2014-06-21 20:41:21 +0200

    mkmap.sh to be executed fr
    The overall use and especially the crontab is simplified by adding the PATH modification to this script and by not requiring a specific location from where mkmap.sh is to be invoked.

--- mkmap.sh
+++ mkmap.sh
@@ -2,14 +2,24 @@
 
 set -e
 
+here=$(dirname $0)
+
 DEST=$1
-
-
 [ "$DEST" ] || exit 1
+
+if [ -x /usr/bin/realpath ]; then
+        #echo -n "I: changed path '$DEST' to "
+        DEST=$(realpath $DEST)
+        #echo "'$DEST'"
+fi
 
 cd "$(dirname "$0")"/
 
 ./ffhlwiki.py http://freifunk.metameute.de/wiki/Knoten > aliases_hl.json
 ./ffhlwiki.py http://freifunk.metameute.de/wiki/Moelln:Knoten > aliases_moelln.json
 
-./bat2nodes.py -A -a aliases.json -a aliases_hl.json -a aliases_moelln.json -d $DEST
+EXTRABIN=$HOME
+
+PATH=$EXTRABIN:$PATH ./bat2nodes.py -A -d $DEST
+
+#./bat2nodes.py -A -a aliases.json -a aliases_hl.json -a aliases_moelln.json -d $DEST
